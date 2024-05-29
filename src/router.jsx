9e137291e2f1c48e4457f162cab1e55a957e0a57
bruno <brunoaugustine2@gmail.com>
2024-05-29 15:46:36 +0100

    second commit

--- src/router.jsx
+++ src/router.jsx
@@ -1,22 +1,8 @@
-// import { createBrowserRouter } from "react-router-dom";
-// import App from "./App";
-// import Settings from "./pages/Settings";
-// import Contact from "./pages/Contact";
-// import Layout from "./components/layout";
-
 import Administration from "./pages/Administration";
 import Dashboard from "./pages/Dashboard";
 import SignUp from "./pages/signUp/SignUp";
 import Home from './pages/Home';
 import SignIn from "./pages/signIn/SignIn";
-
-// import Home from "./pages/layout/Home";
-
-// export const router  = createBrowserRouter([
-//     {path: '/', element: <App />},
-//     {path: '/dashboard', element: <Layout><Settings /></Layout>},
-//     {path: '/contact', element:<Layout> <Contact /></Layout>},
-//   ])
 
 
 export const AuthRoutes = [
